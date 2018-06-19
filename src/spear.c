50b92a036aae7c38c68629cf1407d4dc2419a4cb
zentaro jinno <jinno.z.aa@m.titech.ac.jp>
2018-06-19 18:54:09 +0900

    a

--- src/spear.c
+++ src/spear.c
@@ -4,13 +4,33 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+    int use = 0;
+    int i;
+    for(i = 0 ; i < n ; i++){
+        use = use+A[i]/m;
+    }
+    return use >= k;}
 
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
+  lb = 0;
+  ub = 1000000000;
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+    while(ub-lb>1){
+    int m = (ub+lb)/2;
+  if(p(m)){
+    lb = m;
+}
+else {
+    ub = m;
+}
+}
+printf("%d\n",lb);
+
 
 
   return 0;
