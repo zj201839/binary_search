2258ec7d85783535a2a43bb1526daaba36aeb6fd
zentaro jinno <jinno.z.aa@m.titech.ac.jp>
2018-06-19 18:53:06 +0900

    a

--- src/array.c
+++ src/array.c
@@ -8,9 +8,21 @@ int A[100000];
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
+  lb = 0;
+  ub = n;
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  while(ub-lb>1){
+    int m = (ub+lb)/2;
+    if(A[m]<=k){
+        lb = m;
+    }
+    else{
+        ub = m;
+    }
+}
+ printf("%d\n",ub);       
 
 
 
