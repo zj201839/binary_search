3fdc7fe4fc2a999002682fb091814de042a3401e
zentaro jinno <jinno.z.aa@m.titech.ac.jp>
2018-07-27 16:28:27 +0900

    spear syusei

--- src/spear.c
+++ src/spear.c
@@ -8,28 +8,29 @@ int p(int m){
     int use = 0;
     int i;
     for(i = 0 ; i < n ; i++){
-        use = use+A[i]/m;
+        use += A[i]/m;
     }
-    return use >= k;}
+    return use >= k;
+}
 
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
   lb = 0;
-  ub = 10000000000;
+  ub = 1000000001;
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-    while(ub-lb>1){
+  while(ub-lb>1){
     int m = (ub+lb)/2;
-  if(p(m)){
-    lb = m;
-}
-else {
-    ub = m;
-}
-}
-printf("%d\n",lb);
+    if(p(m)){
+      lb = m;
+    }
+    else {
+      ub = m;
+    }
+  }
+  printf("%d\n",lb);
 
 
 
